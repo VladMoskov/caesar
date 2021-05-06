10b89f2f3be9e84f3120ef9b0e78407460713a6f
vladmoskov <perishvlad@gmail.com>
2021-05-06 19:50:02 +0300

    improve file check logic

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const {pipeline} = require('stream');
 const {Cipher} = require('./lib/Cipher');
-const {options} = require('./lib/comand-line')
+const {input, output} = require('./lib/comand-line')
 
 pipeline(
-    options.input ? fs.createReadStream(options.input) : process.stdin,
+    input ? fs.createReadStream(input) : process.stdin,
     new Cipher,
-    options.output ? fs.createWriteStream(options.output, {flags: 'a'}) : process.stdout,
+    output ? fs.createWriteStream(output, {flags: 'a'}) : process.stdout,
     (err) => {
         if (err) {
-            process.stderr.write('Input file is not exist');
+            process.stderr.write(err);
             process.exit(1);
         } else {
             console.log('Pipeline succeeded.');
+            process.exit(0);
         }
     }
 )
